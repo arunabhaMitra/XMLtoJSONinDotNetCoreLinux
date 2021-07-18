649dec44dd544393c47628f091a6df7cf224821a
Arunabha Mitra <arunabha.official@gmail.com>
2021-07-19 00:48:29 +0530

    Add files via upload
    Working Dot NET Core Console app to convert XML to JSON

---
+++ EmpXMLtoJSONConverter.ConsoleApp/Employee.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace EmpXMLtoJSONConverter.ConsoleApp
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
---
+++ EmpXMLtoJSONConverter.ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace EmpXMLtoJSONConverter.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(workingDirectory + "/SourceFiles/XMLSample.xml");
+            Console.WriteLine("XML ==> " + xmlDoc.InnerXml);
+
+            string empJSON = XMLtoJSON(xmlDoc.InnerXml);
+            Console.WriteLine("\nJSON ==> " + empJSON);
+        }
+
+        public static string XMLtoJSON(string xmlData)
+        {
+            Employee emp;
+            XmlSerializer xmlserializer = new XmlSerializer(typeof(Employee));
+            
+            using(StringReader textReader = new StringReader(xmlData))
+            {
+                emp = (Employee)xmlserializer.Deserialize(textReader);
+            }
+            return EmployeeToJson(emp);
+        }
+        public static string EmployeeToJson(Employee emp)
+        {
+            return JsonSerializer.Serialize(emp);
+        }
+    }
+}
